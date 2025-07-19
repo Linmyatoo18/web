79476b7752dbb0e7419b8763f648a129da3308f3
Linmyatoo <108082128+Linmyatoo18@users.noreply.github.com>
2025-07-19 20:13:14 +0630

    logic

---
+++ script.js
@@ -0,0 +1,50 @@
+function add(){
+    document.getElementById("result").innerHTML = 5+2;
+}
+function subtract(){
+    document.getElementById("result").innerHTML = 5-2;
+}
+function multiply(){
+    document.getElementById("result").innerHTML = 5*2;
+}
+function divide(){
+    document.getElementById("result").innerHTML = 5/2;
+}
+
+//command
+/*hhh
+222
+*/
+
+ x = 5;
+ y = 2;
+ z = x ** y;
+
+console.log(z)
+
+console.log(5e-5)
+
+const pi = 3.14159
+
+console.log(pi)
+
+myObj = {
+  name:"John",
+  age:30,
+  myCars: {
+    car1:"Ford",
+    car2:"BMW",
+    car3:"Fiat"
+  }
+}
+
+console.log(myObj.name)
+
+console.log(myObj.myCars.car2)
+
+let i = temp(5);
+console.log(i)
+
+function temp(num){
+    return 5 * num
+}
