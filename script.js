85b0f6c7e09286ab216cea3fcf554281281ac06d
Linmyatoo_18 <linmyatoo1010@gmail.com>
2025-07-24 22:14:12 +0630

    obj, Properties

--- script.js
+++ script.js
@@ -48,3 +48,51 @@ console.log(i)
 function temp(num){
     return 5 * num
 }
+
+let obj = "Phone";
+const phone = {color:"Black", os:"Android", model:"pixel"};
+console.log(phone.color,phone.os, phone.model);
+
+const book = {
+    title:"C",
+    pages_count: 250,
+    hight: 16,
+    width: 9,
+    color:"BnW",
+    a: function(){
+        return this.title + " " + this.color;
+    }
+};
+
+const Class = new Object();
+
+Class.Name = "A";
+Class.students = 20;
+Class.tables = 10;
+Class.teacher = "John";
+
+document.getElementById("C0").innerHTML = Class.Name;
+
+console.log(Class["teacher"]);
+console.log(Class.teacher);
+console.log(book.a());
+
+document.getElementById("ClassName").innerHTML = "Class : " + Class.Name;
+document.getElementById("ClassStudent").innerHTML = "Students: " + Class.students;
+document.getElementById("ClassTables").innerHTML = "Tables : " + Class.tables;
+document.getElementById("ClassTeacher").innerHTML = "Calss Teacher: " + Class.teacher;
+
+person = {
+    firstname: "John",
+    hight: 170,
+}
+let age = person.age;
+
+
+person.firstname + " is " + person["age"] + " years old.";
+
+let a = "firstname";
+let b = "age";
+person[a] + " is " + person[b] + " years old.";
+
+document.getElementById("person").innerHTML = person[a] + " is " + person[b] + " years old.";
